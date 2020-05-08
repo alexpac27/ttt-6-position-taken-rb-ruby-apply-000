b18ac79a10c9a777f4307ba739500e6d98ce5cb9
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-08 23:38:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken?(board,index)
-  if board[index] == "X" || board[index] == "O"
+  if board[index] == "" || board[index] == " " || board[index] == nil
   elsif board[index] == "X" || board[index] == "O"
     return true
   else
